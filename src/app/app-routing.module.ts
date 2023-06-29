d91f3aa3962f8e66decc3a02abf84b930a2bf752
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-29 23:55:12 +0530

    Feat(Auth): Added routing for auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: "auth",
+		loadChildren: () => import("./modules/auth/auth.module").then(m => m.AuthModule),
+		title: "OmniFlowCRM Authentication",
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from "./auth.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: AuthComponent,
+        title: "Authentication Component",
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
