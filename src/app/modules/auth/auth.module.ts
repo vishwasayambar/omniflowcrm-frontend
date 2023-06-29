b4d86f25f7b2e903bcc0e8bc949bf38e8183526d
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-29 23:19:40 +0530

    Feat(Auth): Created New Auth module and Signup and Login component

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { AuthRoutingModule } from "./auth-routing.module";
+import { AuthComponent } from "./auth.component";
+import { SignupComponent } from "./signup/signup.component";
+import { LoginComponent } from "./login/login.component";
+
+
+@NgModule({
+	declarations: [
+		AuthComponent,
+		SignupComponent,
+		LoginComponent
+	],
+	imports: [
+		CommonModule,
+		AuthRoutingModule
+	]
+})
+export class AuthModule {
+}
