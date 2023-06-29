2ea5dd64496b91c0e33e680a01c4fad2353f9de7
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-29 23:54:21 +0530

    impr: Update Environment File

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: "localhost",
+  frontendBaseUrl: "http://localhost:4200",
+  API_URL: 'http://127.0.0.1:80/api',
 };
 
 /*
