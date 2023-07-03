ef2f3a07a1a0d9b7833607752dd33652d986b709
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-07-03 22:00:18 +0530

    Feat(Auth): Update the routes of auth  module

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from "./auth.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
 const routes: Routes = [
     {
         path: '',
         component: AuthComponent,
         title: "Authentication Component",
+        children:   [
+            {
+                path: 'signUp',
+                component: SignupComponent,
+                title: "OmniFlowCRM signUp Page"
+            },
+            {
+                path: 'login',
+                component: LoginComponent,
+                title: "OmniFlowCRM Login Page"
+            },
+            {
+                path: '',
+                redirectTo: "/auth/login",
+                pathMatch: "full",
+            }
+        ]
     },
 ];
 
