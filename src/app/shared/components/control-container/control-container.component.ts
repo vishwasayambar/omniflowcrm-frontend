69ec933272cd972b6931d86657d5fdb0d522d443
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-07-04 00:18:28 +0530

    Basic(Archi):- Created Control-container for form inputs

---
+++ src/app/shared/components/control-container/control-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-control-container',
+  templateUrl: './control-container.component.html',
+  styleUrls: ['./control-container.component.scss']
+})
+export class ControlContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { SharedDevExtremeModule } from "./dev-extreme.module";
+import { ControlContainerComponent } from "./components/control-container/control-container.component";
 
 const COMMON_MODULE_LIST = [
+	ControlContainerComponent,
 
-]
+];
 
 @NgModule({
-  declarations: [
-    // COMMON_MODULE_LIST,
-
-  ],
-  imports: [
-    CommonModule,
-    SharedDevExtremeModule,
-  ],
-  exports:[
-    // COMMON_MODULE_LIST,
-    SharedDevExtremeModule,
-  ]
+	declarations: [
+		COMMON_MODULE_LIST,
+	],
+	imports: [
+		CommonModule,
+		SharedDevExtremeModule,
+		FormsModule,
+		ReactiveFormsModule,
+	],
+	exports: [
+		COMMON_MODULE_LIST,
+		SharedDevExtremeModule,
+		FormsModule,
+		ReactiveFormsModule,
+	]
 })
-export class SharedModule { }
+export class SharedModule {
+}
