d49c7eb58d6a175ac8a3c41c5628415285c151cf
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-07-08 23:29:14 +0530

    Basic(Archi): Added Form Validetor method in base component

--- src/app/shared/base-component.ts
+++ src/app/shared/base-component.ts
@@ -1,4 +1,5 @@
 import { Injector } from "@angular/core";
+import { UntypedFormArray, UntypedFormControl, UntypedFormGroup } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { AuthService } from "./services/auth.service";
 
@@ -11,4 +12,26 @@ export abstract class BaseComponent{
 		this.activatedRouter = injector.get(ActivatedRoute);
 		this.authService = injector.get(AuthService);
     }
+
+
+	validateFormFields(form: UntypedFormGroup | UntypedFormArray): void {
+		Object.keys(form.controls).forEach(field => {
+			const control = form.get(field);
+			if (control instanceof UntypedFormControl) {
+				control.markAsTouched();
+				control.updateValueAndValidity(); // Reruns the custom validators if any
+			} else if (control instanceof UntypedFormGroup) {
+				this.validateFormFields(control);
+			} else if (control instanceof UntypedFormArray) {
+				Object.keys(control.controls).forEach(index => {
+					if (control instanceof UntypedFormControl) {
+						control.markAsTouched();
+						control.updateValueAndValidity();
+					} else {
+						this.validateFormFields(control.get(index) as UntypedFormGroup);
+					}
+				});
+			}
+		});
+	}
 }
